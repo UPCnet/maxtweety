f580bdd1d23c5a29c9ddc29edc2dbb61738407f9
Carles Bruguera <carlesba@gmail.com>
2013-10-03 17:30:30 +0200

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 ]
 
 setup(name='maxtweety',
-      version='1.1.1',
+      version='1.1.2.dev0',
       description='maxtweety',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
