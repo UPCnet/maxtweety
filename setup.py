07b5d3b3742c2251cae4b3eb85fd3cde6fb43fe2
Carles Bruguera <carlesba@gmail.com>
2014-06-10 15:24:16 +0200

    Back to development: 4.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
 ]
 
 setup(name='maxtweety',
-      version='4.0.1',
+      version='4.0.2.dev0',
       description='maxtweety',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
